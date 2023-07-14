0ba1f576eff9ae3266ea7a324c12047d5d6d37b9
Leandro Bertero <leandro.bertero@hotmail.com>
2023-07-14 18:09:17 -0300

    segundo programa - Formulario inicial

---
+++ Estructuras/Estructuras/Estructuras.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace Estructuras
+{
+    partial class Estructuras
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Estructuras));
+            SuspendLayout();
+            // 
+            // Estructuras
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Red;
+            ClientSize = new Size(800, 450);
+            ForeColor = SystemColors.ActiveCaption;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Estructuras";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Estructuras";
+            Load += Estructuras_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Estructuras/Estructuras/Estructuras.cs
@@ -0,0 +1,15 @@
+namespace Estructuras
+{
+    public partial class Estructuras : Form
+    {
+        public Estructuras()
+        {
+            InitializeComponent();
+        }
+
+        private void Estructuras_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.BlueViolet;
+        }
+    }
+}
