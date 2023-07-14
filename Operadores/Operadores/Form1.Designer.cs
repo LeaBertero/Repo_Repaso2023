a63a465c8847dbe3f8b58f114c4fd1b3f020eb4c
Leandro Bertero <leandro.bertero@hotmail.com>
2023-07-14 12:14:11 -0300

    operadores - formulario de repaso

---
+++ Operadores/Operadores/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Operadores
+{
+    partial class FrmInicio
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            BtCalcular = new Button();
+            Txoper1 = new TextBox();
+            Txoper2 = new TextBox();
+            label1 = new Label();
+            LblResultado = new Label();
+            BtSalir = new Button();
+            SuspendLayout();
+            // 
+            // BtCalcular
+            // 
+            BtCalcular.Cursor = Cursors.Hand;
+            BtCalcular.Location = new Point(805, 210);
+            BtCalcular.Name = "BtCalcular";
+            BtCalcular.Size = new Size(158, 82);
+            BtCalcular.TabIndex = 0;
+            BtCalcular.Text = "Calcular";
+            BtCalcular.UseVisualStyleBackColor = true;
+            // 
+            // Txoper1
+            // 
+            Txoper1.Location = new Point(301, 238);
+            Txoper1.Name = "Txoper1";
+            Txoper1.Size = new Size(125, 27);
+            Txoper1.TabIndex = 1;
+            // 
+            // Txoper2
+            // 
+            Txoper2.Location = new Point(605, 238);
+            Txoper2.Name = "Txoper2";
+            Txoper2.Size = new Size(125, 27);
+            Txoper2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(506, 241);
+            label1.Name = "label1";
+            label1.Size = new Size(19, 20);
+            label1.TabIndex = 3;
+            label1.Text = "+";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // LblResultado
+            // 
+            LblResultado.AutoSize = true;
+            LblResultado.Location = new Point(482, 350);
+            LblResultado.Name = "LblResultado";
+            LblResultado.Size = new Size(75, 20);
+            LblResultado.TabIndex = 4;
+            LblResultado.Text = "Resultado";
+            LblResultado.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // BtSalir
+            // 
+            BtSalir.BackColor = SystemColors.Highlight;
+            BtSalir.Cursor = Cursors.Hand;
+            BtSalir.FlatAppearance.BorderColor = Color.Black;
+            BtSalir.FlatAppearance.BorderSize = 5;
+            BtSalir.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            BtSalir.ForeColor = Color.Ivory;
+            BtSalir.Location = new Point(1047, 459);
+            BtSalir.Name = "BtSalir";
+            BtSalir.Size = new Size(127, 71);
+            BtSalir.TabIndex = 5;
+            BtSalir.Text = "Salir";
+            BtSalir.UseVisualStyleBackColor = false;
+            BtSalir.Click += BtSalir_Click;
+            // 
+            // FrmInicio
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkTurquoise;
+            ClientSize = new Size(1186, 542);
+            Controls.Add(BtSalir);
+            Controls.Add(LblResultado);
+            Controls.Add(label1);
+            Controls.Add(Txoper2);
+            Controls.Add(Txoper1);
+            Controls.Add(BtCalcular);
+            Name = "FrmInicio";
+            Text = "Operadores";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button BtCalcular;
+        private TextBox Txoper1;
+        private TextBox Txoper2;
+        private Label label1;
+        private Label LblResultado;
+        private Button BtSalir;
+    }
+}
---
+++ Operadores/Operadores/Form1.cs
@@ -0,0 +1,15 @@
+namespace Operadores
+{
+    public partial class FrmInicio : Form
+    {
+        public FrmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void BtSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
