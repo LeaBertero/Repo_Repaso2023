073fa4f161c9c9791e6d2455de800a3e6ef163cd
Leandro Bertero <leandro.bertero@hotmail.com>
2023-07-14 23:58:59 -0300

    azul de fondo en el segundo formulario

--- Estructuras/Estructuras/Nuevo.Designer.cs
+++ Estructuras/Estructuras/Nuevo.Designer.cs
@@ -48,6 +48,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DeepSkyBlue;
             ClientSize = new Size(800, 450);
             Controls.Add(BtSalir2);
             Name = "FrmNuevo";
