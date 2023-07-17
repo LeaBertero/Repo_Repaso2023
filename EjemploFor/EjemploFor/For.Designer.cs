ddf070880ff8658197e1ff2090936bdf23b69bf3
Leandro Bertero <leandro.bertero@hotmail.com>
2023-07-17 01:48:18 -0300

    Ejemplo for - Primera parte

---
+++ EjemploFor/EjemploFor/For.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace EjemploFor
+{
+    partial class FrmInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmInicio));
+            SuspendLayout();
+            // 
+            // FrmInicio
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Orange;
+            ClientSize = new Size(964, 529);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "FrmInicio";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Ejemplo For";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
