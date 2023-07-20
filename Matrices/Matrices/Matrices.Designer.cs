69fa86a58c02b958a45e27df7f6bf3d1874e65aa
Leandro Bertero <leandro.bertero@hotmail.com>
2023-07-20 12:29:07 -0300

    Matrices - inicio

--- Matrices/Matrices/Matrices.Designer.cs
+++ Matrices/Matrices/Matrices.Designer.cs
@@ -28,18 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(405, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(275, 81);
+            label1.TabIndex = 0;
+            label1.Text = "Matrices";
             // 
             // Matrices
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.GreenYellow;
             ClientSize = new Size(1111, 590);
+            Controls.Add(label1);
             Name = "Matrices";
             Text = "Matrices";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
