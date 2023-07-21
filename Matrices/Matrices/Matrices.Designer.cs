129ef7507688ec31c519e7086a3fe9dc6b82a1f0
Leandro Bertero <leandro.bertero@hotmail.com>
2023-07-21 13:14:09 -0300

    Matrices con try catch,
    hecho con try catch para controlar el formulario  que no se rompa con el limite de carga  de personas

--- Matrices/Matrices/Matrices.Designer.cs
+++ Matrices/Matrices/Matrices.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            TxIngreso = new TextBox();
+            LblRes = new Label();
+            BtInicio = new Button();
+            lblMensaje = new Label();
+            BtSalir = new Button();
             SuspendLayout();
             // 
             // label1
@@ -41,14 +46,72 @@
             label1.TabIndex = 0;
             label1.Text = "Matrices";
             // 
+            // TxIngreso
+            // 
+            TxIngreso.Location = new Point(135, 146);
+            TxIngreso.Name = "TxIngreso";
+            TxIngreso.Size = new Size(483, 27);
+            TxIngreso.TabIndex = 1;
+            // 
+            // LblRes
+            // 
+            LblRes.AutoSize = true;
+            LblRes.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            LblRes.Location = new Point(135, 226);
+            LblRes.Name = "LblRes";
+            LblRes.Size = new Size(0, 32);
+            LblRes.TabIndex = 2;
+            // 
+            // BtInicio
+            // 
+            BtInicio.Location = new Point(677, 144);
+            BtInicio.Name = "BtInicio";
+            BtInicio.Size = new Size(94, 29);
+            BtInicio.TabIndex = 3;
+            BtInicio.Text = "comenzar";
+            BtInicio.UseVisualStyleBackColor = true;
+            BtInicio.Click += BtInicio_Click;
+            // 
+            // lblMensaje
+            // 
+            lblMensaje.AutoSize = true;
+            lblMensaje.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblMensaje.ForeColor = Color.White;
+            lblMensaje.Location = new Point(135, 554);
+            lblMensaje.Name = "lblMensaje";
+            lblMensaje.Size = new Size(0, 32);
+            lblMensaje.TabIndex = 4;
+            lblMensaje.Visible = false;
+            // 
+            // BtSalir
+            // 
+            BtSalir.BackColor = Color.YellowGreen;
+            BtSalir.Cursor = Cursors.Hand;
+            BtSalir.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            BtSalir.ForeColor = SystemColors.ButtonHighlight;
+            BtSalir.Location = new Point(931, 565);
+            BtSalir.Name = "BtSalir";
+            BtSalir.Size = new Size(168, 51);
+            BtSalir.TabIndex = 5;
+            BtSalir.Text = "Salir";
+            BtSalir.UseVisualStyleBackColor = false;
+            BtSalir.Click += BtSalir_Click;
+            // 
             // Matrices
             // 
+            AcceptButton = BtInicio;
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.GreenYellow;
-            ClientSize = new Size(1111, 590);
+            ClientSize = new Size(1111, 628);
+            Controls.Add(BtSalir);
+            Controls.Add(lblMensaje);
+            Controls.Add(BtInicio);
+            Controls.Add(LblRes);
+            Controls.Add(TxIngreso);
             Controls.Add(label1);
             Name = "Matrices";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Matrices";
             ResumeLayout(false);
             PerformLayout();
@@ -57,5 +120,10 @@
         #endregion
 
         private Label label1;
+        private TextBox TxIngreso;
+        private Label LblRes;
+        private Button BtInicio;
+        private Label lblMensaje;
+        private Button BtSalir;
     }
 }
--- Matrices/Matrices/Matrices.cs
+++ Matrices/Matrices/Matrices.cs
@@ -2,9 +2,41 @@ namespace Matrices
 {
     public partial class Matrices : Form
     {
+        //lista de arreglos
+        string[] Lista = new string[10];
+        int posicion = 0;
+
         public Matrices()
         {
             InitializeComponent();
+        }
+
+        private void BtInicio_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Lista[posicion] = TxIngreso.Text;
+                posicion = posicion + 1;
+
+                LblRes.Text = LblRes.Text + TxIngreso.Text + "\r\n";
+
+                TxIngreso.Text = "";
+                TxIngreso.Focus();
+            }
+            catch (Exception)
+            {
+
+                lblMensaje.Text = "Limite de carga, completo";
+                lblMensaje.BackColor = Color.Black;
+                lblMensaje.Visible = true;
+            }
+
+
+        }
+
+        private void BtSalir_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
