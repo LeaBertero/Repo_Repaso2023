473f5101d9e71701582dfe6e79f346bce29309f8
Leandro Bertero <leandro.bertero@hotmail.com>
2023-07-23 22:22:26 -0300

    codigo de lista de precios
    primera parte

--- ListaPrecio/ListaPrecio/Lista-precio.cs
+++ ListaPrecio/ListaPrecio/Lista-precio.cs
@@ -12,6 +12,12 @@ namespace ListaPrecio
 {
     public partial class Lista_precio : Form
     {
+        //matrices
+        private string[,] Producto = new string[3, 2];
+        private decimal[] Precio = new decimal[3];
+
+        private int Fila = 0;
+        
         public Lista_precio()
         {
             InitializeComponent();
@@ -27,6 +33,18 @@ namespace ListaPrecio
         private void BtAceptar_Click(object sender, EventArgs e)
         {
             BtAceptar.Focus();
+
+            //matriz bidimensional
+            Producto[Fila, 0] = TxCodigo.Text;
+            Producto[Fila, 1] = TxProducto.Text;
+
+            //Matriz unidimensional
+            Precio[Fila] = Convert.ToDecimal(TxPrecio.Text);
+
+            //contador de fila, para pasar a ka siguiente carga de producto
+            Fila = Fila + 1;
+
+
         }
 
         private void BtListar_Click(object sender, EventArgs e)
