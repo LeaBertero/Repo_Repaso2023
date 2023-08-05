a7d67da88e8a3f0984a7398d63d9c7a352e8034e
Leandro Bertero <leandro.bertero@hotmail.com>
2023-08-05 02:47:25 -0300

    do while - inicio

---
+++ DoWhile/DoWhile/Dowhile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoWhile
+{
+    public partial class Dowhile : Form
+    {
+        public Dowhile()
+        {
+            InitializeComponent();
+        }
+    }
+}
