1163f5823250e7aab98cf3b377f3cf6592e3dffe
Leandro Bertero <leandro.bertero@hotmail.com>
2023-08-06 19:57:48 -0300

    Proyecto Try_(Prueba)
    commit inicial

---
+++ Try/Try/Try.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Try
+{
+    partial class Try
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Try
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.CornflowerBlue;
+            ClientSize = new Size(1103, 566);
+            Name = "Try";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Try";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
